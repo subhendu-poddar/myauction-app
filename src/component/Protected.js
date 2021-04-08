cf9c65677aa9ff424f08cbc9ed264edcc22663d5
subhendu-poddar <spoddar4444@gmail.com>
2021-04-08 16:31:45 +0530

    updated

--- src/component/Protected.js
+++ src/component/Protected.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { Redirect } from "react-router-dom";
 
 class Protected extends Component {
+    
     render() {
-        var teamLoggedin = JSON.parse(localStorage.getItem('team'));
+        var TeamLoggedin = JSON.parse(localStorage.getItem('team'));
+        var AdminLoggedin = JSON.parse(localStorage.getItem('admin'));
+        var PlayerLoggedin = JSON.parse(localStorage.getItem('player'));
         return (
             <div>
-                {teamLoggedin && <Redirect to="/TeamPage" /> }
+                {TeamLoggedin && <Redirect to="/TeamPage" />}
+                {AdminLoggedin && <Redirect to="/AdminPage" />}
+
             </div>
         );
     }
