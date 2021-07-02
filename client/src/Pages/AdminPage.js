e28c7aeff0fc157f2d4ff6d4a91f41ba4191c11d
subhendu-poddar <spoddar4444@gmail.com>
2021-07-02 12:22:09 +0530

    reverted

--- client/src/Pages/AdminPage.js
+++ client/src/Pages/AdminPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { hashRouter, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Players from "../Dashboard/Players"
 import Navigation from "./Navigation/AdminNavigation"
 import Leaderboard from "../Dashboard/Leaderboard"
@@ -30,7 +30,7 @@ class AdminPage extends Component {
         </div>
 
         <div>
-          <hashRouter>
+          <BrowserRouter>
             <Navigation />
 
             <Switch>
@@ -40,7 +40,7 @@ class AdminPage extends Component {
               <Route path="/"> {null} </Route>
 
             </Switch>
-          </hashRouter>
+          </BrowserRouter>
         </div>
 
       </div>
--- client/src/Pages/PlayerPage.js
+++ client/src/Pages/PlayerPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { hashRouter, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Players from "../Dashboard/Players"
 import Navigation from "./Navigation/PlayerNavigation"
 import Leaderboard from "../Dashboard/Leaderboard"
@@ -30,7 +30,7 @@ class TeamPage extends Component {
         </div>
 
         <div>
-          <hashRouter>
+          <BrowserRouter>
             <Navigation />
 
             <Switch>
@@ -41,7 +41,7 @@ class TeamPage extends Component {
               <Route path="/"> {null} </Route>
 
             </Switch>
-          </hashRouter>
+          </BrowserRouter>
         </div>
 
       </div>
