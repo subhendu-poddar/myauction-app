bdd112e35f241bb80afbc5d805a9b19601cda1a2
subhendu-poddar <spoddar4444@gmail.com>
2021-07-02 11:53:25 +0530

    added hashRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import dotenv from 'dotenv';
 import Home from './component/Home'
 import Login from './component/TeamLogin'
@@ -33,7 +33,6 @@ function App() {
           <Route exact path='/admin/signin'> <Admin/> </Route>
           <Route exact path='/TeamPage'> <TeamPage/> </Route>
           <Route exact path='/PlayerPage'> <PlayerPage/> </Route>
-          <Route exact path='/'> <Home/> </Route>
           
         </Switch>
 
